61ac05b40abf74d8ef836d5ec16e6a668c1964c4
江心才子 <270628297@qq.com>
2019-05-21 01:00:57 +0800

    添加协程依赖

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+/*
+plugins {
+    java
+    kotlin("jvm") version "1.3.31"
+}
+
+group = "com.wyl.kotlin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk7"))
+    testImplementation("junit:junit:4.12")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
+}*/
---
+++ src/main/kotlin/com/wyl/kotlin/_01coroutines/01第一个协程.kt
@@ -0,0 +1,15 @@
+package com.wyl.kotlin._01coroutines
+
+
+/**
+ * 2019年5月21日 00:33:32
+ *
+ */
+fun main() {
+    println("主线程开始执行")
+
+
+    println("主线程执行结束")
+
+
+}
